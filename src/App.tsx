a17a73cc31daf9c5b5f7bfadffc16730aa521ed0
herbertDev12 <herbertn740@gmail.com>
2025-04-29 17:34:05 -0700

    Created the ui folder and added a bold title for a title below HeaderBar

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,12 @@ export default function App() {
   return (
     <>
       <HeaderBar />
-      <main className="pt-24 lg:pt-32"> 
-        Contenidos dw
-      </main>
+      <div className="fixed z-50 
+        top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2
+        text-center text-gray-800 text-2xl font-bold">
+        Extensions List
+      </div>
+
     </>
   )
 }
--- src/Components/HeaderBar.tsx
+++ src/Components/HeaderBar.tsx
@@ -1,5 +1,3 @@
-
-
 export default function HeaderBar() {
   return (
     <header className="
