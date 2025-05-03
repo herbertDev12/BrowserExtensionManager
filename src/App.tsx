fa7b28b8e1cd356e7e7bfdb3de096d7eea929d18
herbertDev12 <herbertn740@gmail.com>
2025-05-02 22:02:01 -0700

    Created the FilterBar component and added the CustomButton (Static)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import './App.css'
 import HeaderBar from './Components/HeaderBar'
-import CustomButton from './Components/ui/CustomButton'
+import FiterBar from './Components/FilterBar'
 
 export default function App() {
   return (
-    <body className="min-h-screen bg-blue-50"> 
+    <div> 
       <HeaderBar />
       <div className="fixed z-50 
-        top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2
-        text-center text-gray-800 text-2xl font-bold">
+        top-1/5 left-1/2 transform -translate-x-1/2 -translate-y-1/2
+        text-center text-gray-800 text-2xl font-bold ">
         Extensions List
       </div>
-      <div className="flex justify-center items-center h-screen w-full">
-        <CustomButton />
-      </div>
-    </body>
+      
+      <FiterBar/>    
+    </div>
 
   )
 }
---
+++ src/Components/FilterBar.tsx
@@ -0,0 +1,9 @@
+import CustomButton from "./ui/CustomButton";
+
+export default function FiterBar(){
+    return (
+        <div className="flex justify-center items-center h-screen w-full">
+            <CustomButton></CustomButton>
+        </div>
+    )
+}
