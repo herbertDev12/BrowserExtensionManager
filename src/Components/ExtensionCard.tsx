7407ce34a08b00b75e7c18947d9c5701cd0e5a73
herbertDev12 <herbertn740@gmail.com>
2025-05-06 14:25:29 -0700

    Added the static CustomButton and a dynamic img, title and description to the ExtensionCard

--- src/Components/ExtensionCard.tsx
+++ src/Components/ExtensionCard.tsx
@@ -1,10 +1,45 @@
-export default function ExtensionCard(){
-    return (
-        <div className="w-96 h-48 bg-white rounded-xl shadow-md ">
-          <div className="flex top-4 left-4 right-4 ">
-            <img 
-                src="/src/assets/images/logo-devlens.svg"/>
-          </div>
-        </div>
-      );
+import CustomButton from "./ui/CustomButton";
+
+interface ExtensionCardProps {
+  cardName: string;
+  imageSrc: string;
+  cardDescription: string;
+}
+
+export default function ExtensionCard({
+  cardName,
+  imageSrc,
+  cardDescription
+}: ExtensionCardProps) {
+  return (
+    <div className="w-85 min-h-48 bg-white rounded-xl shadow-md p-6 hover:shadow-lg transition-shadow duration-300 
+                   grid grid-cols-[auto_1fr] grid-rows-[auto_auto_auto] gap-x-4 relative">
+      
+      <div className="row-span-2">
+        <img 
+          src={imageSrc}
+          alt={cardName}
+          className="w-16 h-16 object-contain rounded-lg"
+        />
+      </div>
+
+      
+      <h3 className="text-xl font-semibold text-gray-800 col-start-2">
+        {cardName}
+      </h3>
+      
+      <p className="text-gray-600 text-sm leading-relaxed col-start-2 
+                   -pl-4 border-l-2 border-white -mt-6">
+        {cardDescription}
+      </p>
+
+      <div className="absolute bottom-4 left-[calc(5px_+_10px)]"> 
+        
+        <CustomButton
+          buttonName="Remove"
+          
+        />
+      </div>
+    </div>
+  );
 }
--- src/Components/ExtensionGrid.tsx
+++ src/Components/ExtensionGrid.tsx
@@ -3,7 +3,10 @@ import ExtensionCard from "./ExtensionCard"
 export default function ExtensionGrid(){
     return (
         <div>
-            <ExtensionCard/>
+            <ExtensionCard 
+                imageSrc="src\assets\images\logo-devlens.svg"
+                cardName="DevLens"
+                cardDescription="Quickly inspect page layouts and visualize element boundaries"/>
         </div>  
     )
 }
